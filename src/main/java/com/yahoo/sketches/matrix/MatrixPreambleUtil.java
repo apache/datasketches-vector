fb2124bab582c5329056a4c05ff37dc68750bae7
jmalkin <jmalkin@yahoo-inc.com>
2017-10-05 16:01:31 -0700

    bump ojalgo version, remove a couple hard-coded 8s

--- src/main/java/com/yahoo/sketches/matrix/MatrixPreambleUtil.java
+++ src/main/java/com/yahoo/sketches/matrix/MatrixPreambleUtil.java
@@ -197,9 +197,9 @@ public final class MatrixPreambleUtil {
    */
   static int getAndCheckPreLongs(final Memory mem) {
     final long cap = mem.getCapacity();
-    if (cap < 8) { throwNotBigEnough(cap, 8); }
+    if (cap < 8) { throwNotBigEnough(cap, Long.BYTES); }
     final int preLongs = mem.getByte(0) & 0x3F;
-    final int required = Math.max(preLongs << 3, 8);
+    final int required = Math.max(preLongs << 3, Long.BYTES);
     if (cap < required) { throwNotBigEnough(cap, required); }
     return preLongs;
   }
